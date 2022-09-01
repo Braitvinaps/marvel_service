6bd3c04ceaa629ebf534e39baca19fb56b3b1fda
Alexy <britvinaps@gmail.com>
2022-09-01 16:04:01 +0300

    RndChar

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -18,7 +18,7 @@ class RandomChar extends React.Component {
 
     componentDidMount = () => {
         this.updateChar();
-        this.timerId = setInterval(this.updateChar, 15000);
+        // this.timerId = setInterval(this.updateChar, 15000);
     }
 
     componentWillUnmount = () => {
@@ -32,6 +32,12 @@ class RandomChar extends React.Component {
         });
     }
 
+    onCharLoading = () => {
+        this.setState({
+            loading: true
+        })
+    }
+
     onError = () => {
         this.setState({  
             loading: false,
@@ -41,6 +47,7 @@ class RandomChar extends React.Component {
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+        this.onCharLoading();
         this.marvelService
             .getCharacter(id)
             .then(this.onCharLoaded)
